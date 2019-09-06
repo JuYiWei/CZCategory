8ba453bc8748cd64ad9faccfbc7089a8bbe33369
juyw <757832801@qq.com>
2019-09-06 18:36:27 +0800

    0.0.4

---
+++ CZCategory/CZCategory/Category/UIKit/UIScreen+cz.h
@@ -0,0 +1,37 @@
+//
+//  UIScreen+cz.h
+//  CZCategory
+//
+//  Created by juyiwei on 2019/9/6.
+//  Copyright © 2019 居祎炜. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// 屏幕宽高
+#define CZScreenWidth   ([UIScreen mainScreen].bounds.size.width)
+#define CZScreenHeight  ([UIScreen mainScreen].bounds.size.height)
+#define CZScreenScale   ([UIScreen mainScreen].scale)
+
+// 屏幕区分 iPhone
+#define CZScreenPhone_Max_1242x2688     ([[UIScreen mainScreen] respondsToSelector:@selector(currentMode)] ? CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(1242, 2688)) : NO)
+#define CZScreenPhone_X_1242x2208       ([[UIScreen mainScreen] respondsToSelector:@selector(currentMode)] ? CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(1242, 2208)) : NO)
+#define CZScreenPhone_Plus_1125x2436    ([[UIScreen mainScreen] respondsToSelector:@selector(currentMode)] ? CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(1125, 2436)) : NO)
+#define CZScreenPhone_Xr_828x1792       ([[UIScreen mainScreen] respondsToSelector:@selector(currentMode)] ? CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(828, 1792)) : NO)
+#define CZScreenPhone_6_750x1334        ([[UIScreen mainScreen] respondsToSelector:@selector(currentMode)] ? CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(750, 1334)) : NO)
+#define CZScreenPhone_5_640x1136        ([[UIScreen mainScreen] respondsToSelector:@selector(currentMode)] ? CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(640, 1136)) : NO)
+#define CZScreenPhone_4_640x960         ([[UIScreen mainScreen] respondsToSelector:@selector(currentMode)] ? CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(640, 960)) : NO)
+
+// 屏幕区分 iPad
+#define CZScreenPad_12_9_2048x2732      ([[UIScreen mainScreen] respondsToSelector:@selector(currentMode)] ? CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(2048, 2732)) : NO)
+#define CZScreenPad_10_5_1668x2224      ([[UIScreen mainScreen] respondsToSelector:@selector(currentMode)] ? CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(1668, 2224)) : NO)
+#define CZScreenPad_9_7_1536x2048       ([[UIScreen mainScreen] respondsToSelector:@selector(currentMode)] ? CGSizeEqualToSize([UIScreen mainScreen].currentMode.size, CGSizeMake(1536, 2048)) : NO)
+
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIScreen (cz)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ CZCategory/CZCategory/Category/UIKit/UIScreen+cz.m
@@ -0,0 +1,13 @@
+//
+//  UIScreen+cz.m
+//  CZCategory
+//
+//  Created by juyiwei on 2019/9/6.
+//  Copyright © 2019 居祎炜. All rights reserved.
+//
+
+#import "UIScreen+cz.h"
+
+@implementation UIScreen (cz)
+
+@end
